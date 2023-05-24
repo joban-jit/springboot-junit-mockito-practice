aa223974094ccd8c2e7b41d1d364f2af0877c077
Jobanjit Singh <jbnjeet@gmail.com>
2023-05-24 12:03:39 +0530

    defined port config

--- src/test/java/com/springboot/application/testing/ui/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/com/springboot/application/testing/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,8 +1,27 @@
 package com.springboot.application.testing.ui.controllers;
 
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(locations = "/application-test.properties",
+        properties = "server.port=8081")
+//@TestPropertySource(locations = "/application-test.properties")
+//  properties = {"server.port=8081", "hostname=192.168.0.2"} to override properties with @SpringBootTest and @TestPropertySource annotation
+// SpringBootTest.WebEnvironment.MOCK env is the default webEnvironment,
+// and in that it doesn't run the real embedded server container
+// so you use MockMvc to test you app
+
 class UsersControllerIntegrationTest {
-    
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Test
+    void contextLoad() {
+        System.out.println("server port: " + serverPort);
+    }
+
 }
